990e2747a3c4e6a18ade7acc4ec42b6b01d22bc3
Сухачева Валерия <31817397+Sukhacheva@users.noreply.github.com>
2020-06-26 22:08:03 +0300

    Create 31.py

---
+++ 31.py
@@ -0,0 +1,19 @@
+print('-'*45)
+print(chr(124) + "  A  "+chr(124)+"  B"+'  '+ chr(124)+ '  '+'A and B  ' + chr(124)+"  "+ "A or B"+'  '+ chr(124)+ '  '+'A->B  '+chr(124))
+print('-'*45)
+a = False
+b = False
+print(chr(124)+ '  ' + str(int(a))+ '  '+chr(124)+'  '+ str(int(b))+'  '+ chr(124)+ '     '+str(int(a and b))+ '     ' + chr(124)+'     '+ str(int(a or b))+'    '+ chr(124)+ '   '+str(int(not a or b))+ '    '+chr(124))
+print('-'*45)
+a = False
+b = True
+print(chr(124)+ '  ' + str(int(a))+ '  '+chr(124)+'  '+ str(int(b))+'  '+ chr(124)+ '     '+str(int(a and b))+ '     ' + chr(124)+'     '+ str(int(a or b))+'    '+ chr(124)+ '   '+str(int(not a or b))+ '    '+chr(124))
+print('-'*45)
+a = True
+b = False
+print(chr(124)+ '  ' + str(int(a))+ '  '+chr(124)+'  '+ str(int(b))+'  '+ chr(124)+ '     '+str(int(a and b))+ '     ' + chr(124)+'     '+ str(int(a or b))+'    '+ chr(124)+ '   '+str(int(not a or b))+ '    '+chr(124))
+print('-'*45)
+a = True
+b = True
+print(chr(124)+ '  ' + str(int(a))+ '  '+chr(124)+'  '+ str(int(b))+'  '+ chr(124)+ '     '+str(int(a and b))+ '     ' + chr(124)+'     '+ str(int(a or b))+'    '+ chr(124)+ '   '+str(int(not a or b))+ '    '+chr(124))
+print('-'*45)
